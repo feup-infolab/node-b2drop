5962b0d8c2f7605203c169e7aa788b41ed1a3651
João Rocha <joaorosilva@gmail.com>
2018-01-13 22:49:48 +0000

    Reverted again...

--- test/mockData/files/docMockFile.js
+++ test/mockData/files/docMockFile.js
@@ -1,21 +1,20 @@
-const util = require("../../../src/util");
-
+const Pathfinder = global.Pathfinder;
 const md5File = require("md5-file");
 const uuid = require("uuid");
 const mkdirp = require("mkdirp");
 const rimraf = require("rimraf");
 
-const tempFolder = util.pathInApp("/test/mockData/files/test_downloads");
+const tempFolder = global.util.pathInApp("/test/mockData/files/test_downloads");
 
 rimraf.sync(tempFolder);
 mkdirp.sync(tempFolder);
 
 module.exports = {
-    md5: md5File.sync(util.pathInApp("/test/mockData/files/test_uploads/docTest.doc")),
+    md5: md5File.sync(global.util.pathInApp("/test/mockData/files/test_uploads/docTest.doc")),
     name: "docTest_" + uuid.v4() + ".doc",
     extension: "doc",
-    location: util.pathInApp("/test/mockData/files/test_uploads/docTest.doc"),
-    download_location: util.pathInApp("/test/mockData/files/test_downloads/" + "docTest_" + uuid.v4() + ".doc"),
+    location: global.util.pathInApp("/test/mockData/files/test_uploads/docTest.doc"),
+    download_location: global.util.pathInApp("/test/mockData/files/test_downloads/" + "docTest_" + uuid.v4() + ".doc"),
     metadata: [{
         prefix: "nie",
         shortName: "plainTextContent",
